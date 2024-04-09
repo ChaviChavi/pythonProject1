7117cb1cdf0be6a8ae910213455c7d7a6787e145
ChaviChavi <mylnikovpetr@mail.ru>
2024-04-09 15:05:42 +0300

    def abb new

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 import re
 
-def abbreviate(phrase ):
-    pass
+
+def abbreviate(phrase):
+    one = r'\b[a-zA-Z]+\b'
+    pattern = fr'({one})'
+    word = []
+    for c in re.findall(pattern,phrase):
+        word.append(c[0].upper())
+        for x in c[1:]:
+            if x.isupper():
+                word.append(x)
+    return ''.join(word)
+
+
+
+
 
 
 
